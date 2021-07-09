bb7b10a4e24d39977f5d5fcb218ec4303e8a1f5f
Chris <ckc_80@outlook.com>
2021-07-08 22:30:03 -0600

    Got color options to work for drop down menu

--- js/script.js
+++ js/script.js
@@ -1,30 +1,69 @@
-const nameField = document.getElementById('name');
-const otherJobRole = document.getElementById('other-job-role');
-const jobTitle = document.getElementById('title');
-const tshirtColor = document.getElementById('color');
-const designChoice = document.getElementById('design');
+//Declare all elements
+const nameField = document.getElementById("name");
+const otherJobRole = document.getElementById("other-job-role");
+const jobTitle = document.getElementById("title");
+const tshirtColor = document.getElementById("color");
+const designChoice = document.getElementById("design");
+const activities = document.querySelector('#activities');
 
-
-
+//sets focus to start at Name field on startup
 nameField.focus();
-otherJobRole.style.display = 'none';
 
+//hides other job role feild
+otherJobRole.style.display = "none";
 
 // Display Other job role field when "other" is selected
-jobTitle.addEventListener("change", e => {
-    if (e.target.value == "other") {
-        otherJobRole.style.display = "";
-    } else {
-        otherJobRole.style.display = "none";
+jobTitle.addEventListener("change", (e) => {
+  if (e.target.value == "other") {
+    otherJobRole.style.display = "";
+  } else {
+    otherJobRole.style.display = "none";
+  }
+});
+
+//disables dropdown menu for color on startup
+tshirtColor.disabled = true;
+
+designChoice.addEventListener("change", (e) => {
+  if (e.target.value == "heart js") {
+    //enables dropdown menu, and if something was previously selected resets menu to default "select a design theme above".
+    tshirtColor.disabled = false;
+    tshirtColor.selectedIndex = "0";
+
+    //loops through the child nodes and displays the I "heart" Js shirts only
+    for (let i = 9; i <= 13; i += 2) {
+      tshirtColor.childNodes[i].style.display = "";
     }
-} )
+    //loops though the child nodes and hides the "js puns shirts"
+    for (let i = 3; i <= 7; i += 2) {
+      tshirtColor.childNodes[i].style.display = "none";
+    }
+  }
 
-tshirtColor.disabled = true; 
+  if (e.target.value == "js puns") {
+    //enables dropdown menu, and if something was previously selected resets menu to default "select a design theme above".
+    tshirtColor.disabled = false;
+    tshirtColor.selectedIndex = "0";
 
-designChoice.addEventListener("change", e => {
-   if (e.target.value == 'js puns') {
-       tshirtColor.disabled = false; 
-        
+    // loops through and shows the "js puns" shirts
+    for (let i = 3; i <= 7; i += 2) {
+      tshirtColor.childNodes[i].style.display = "";
+    }
 
-   }
+    //loops through and hides the "i heart js" shirts
+    for (let i = 9; i <= 13; i += 2) {
+      tshirtColor.childNodes[i].style.display = "none";
+    }
+  }
+});
+
+/*
+
+    Checkbox and cost total
+
+*/
+
+activities.addEventListener('change', e => {
+    console.log(activities.getAttribute('data-cost'));
 })
+
